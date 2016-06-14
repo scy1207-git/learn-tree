157638063446a7cc63377044eba323bc12814f6d
leosong <scy1207_it@163.com>
2016-06-14 13:19:20 +0800

    update

---
+++ src/main/java/com/tester/learn/books/javastructure/CH08/CH08_03/CH08_03.java
@@ -0,0 +1,16 @@
+package com.tester.learn.books.javastructure.CH08.CH08_03;
+
+/**
+ * Created by leosong on 16-6-14.
+ */
+public class CH08_03 {
+
+    int data[] = new int[]{9,7,5,3,4,6};
+
+    public static void main(String args[])
+    {
+
+    }
+
+
+}
