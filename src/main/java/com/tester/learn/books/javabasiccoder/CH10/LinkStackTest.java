3135f9ad7192cbd6910902f2be09f52c218908b0
leosong <scy1207_it@163.com>
2016-07-11 14:09:25 +0800

    update

---
+++ src/main/java/com/tester/learn/books/javabasiccoder/CH10/LinkStackTest.java
@@ -0,0 +1,24 @@
+package com.tester.learn.books.javabasiccoder.CH10;
+
+/**
+ * Created by leosong on 16-7-11.
+ */
+public class LinkStackTest {
+
+   public static void main(String[] args)
+   {
+       LinkStack<String> stack =  new LinkStack<String>();
+
+       stack.push("aaaa");
+       stack.push("bbbb");
+       stack.push("cccc");
+       stack.push("dddd");
+
+       System.out.println(stack);
+
+       System.out.println(stack);
+
+   }
+
+
+}
---
+++ src/main/java/com/tester/learn/books/javabasiccoder/CH10/SequenceQueue.java
@@ -0,0 +1,88 @@
+package com.tester.learn.books.javabasiccoder.CH10;
+
+import java.util.Arrays;
+
+/**
+ * Created by leosong on 16-7-11.
+ */
+public class SequenceQueue<T> {
+
+    private int DEFAULT_SIZE =10;
+    private int capacity;
+    private Object[] elementData;
+
+    private int front = 0;
+    private int rear = 0;
+
+    public SequenceQueue()
+    {
+        capacity = DEFAULT_SIZE;
+        elementData = new Object[capacity];
+    }
+
+    public SequenceQueue(T element)
+    {
+        this();
+        elementData[0] = element;
+        rear++;
+    }
+
+    public SequenceQueue(T element, int initSize)
+    {
+        this.capacity = initSize;
+        elementData =new Object[capacity];
+        elementData[0] = element;
+        rear++;
+    }
+
+    public int length()
+    {
+        return rear - front;
+    }
+
+    public void add(T element)
+    {
+        if (rear > capacity -1)
+        {
+            throw new IndexOutOfBoundsException("queue has been full!");
+        }
+        elementData[rear++] = element;
+    }
+
+    public boolean empty(){
+        return rear == front;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T remove()
+    {
+        if (empty())
+        {
+            throw new IndexOutOfBoundsException("empty queue");
+        }
+        T oldValue = (T) elementData[front];
+        elementData[front++] = null;
+        return oldValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T element()
+    {
+        if (empty())
+        {
+            throw new IndexOutOfBoundsException("empty eqeue exception");
+        }
+        return (T)elementData[front];
+    }
+
+
+    public void clear()
+    {
+        Arrays.fill(elementData,null);
+        front = 0;
+        rear = 0;
+    }
+
+
+
+}
